e6964885f1b2c81793d8a38df7d6f1ee7a22691d
ilya5006 <samarcev.ilya@mail.ru>
2019-10-19 17:09:39 +0300

    Some optimization + file with articles

--- parser.py
+++ parser.py
@@ -19,14 +19,15 @@ res = open("./Результат.txt", "a")
 session = Session()
 
 for i in range(len(articles)):
+    currentArticle = articles[i].strip()
     request = session.get("http://www.komus.ru/product/" + articles[i] + "/")
     soup = bs(request.content, "html.parser")
     try:
         span = soup.find("span", attrs={"class": "i-fs30 i-fwb"}).text.strip()
-        print(articles[i].strip() + " " + span)
-        res.write(articles[i].strip() + " " + span + "\n")
+        print(currentArticle + " " + span)
+        res.write(currentArticle + " " + span + "\n")
     except AttributeError:
-        print("Страницы с таким продуктом не существует, либо ценник не найден")
-        res.write(articles[i].strip() + " null\n")
+        print(currentArticle + " null\n")
+        res.write(currentArticle + " null\n")
 
 input('Нажмите Enter, чтобы выйти из программы')
